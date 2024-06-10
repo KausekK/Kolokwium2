447b29e98d31d9a1345e5fe7c5195c61bb87ff52
Kausekk <jakub.kausa@gmail.com>
2024-06-10 20:57:38 +0200

    add project structure

---
+++ Kolokwium2/Kolokwium2/Program.cs
@@ -0,0 +1,28 @@
+using Kolokwium2.Data;
+using Kolokwium2.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddScoped<Service>();
+builder.Services.AddDbContext<Context>(
+    options => options.UseSqlServer("Name=ConnectionStrings:Default"));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+
+app.Run();
+
